a920026a26ebee62ad0208aaf8aa711278897f7b
TR <tom06rowe@gmail.com>
2025-02-18 22:48:15 +0000

    First Commit!

---
+++ busybee.py
@@ -0,0 +1,7 @@
+#This is a dictionary, which is a collection of key-value pairs. The key is the name of the variable, and the value is the value of the variable.
+
+places = {1 : 'A', 2 : 'B', 3 : 'C', 4 : 'D', 5 : 'E', 6 : "F", 7 : 'G', 8 : 'H', 9 : 'I'}
+
+board = f"{places[1]}|{places[2]}|{places[3]}\n{places[4]}|{places[5]}|{places[6]}\n{places[7]}|{places[8]}|{places[9]}"
+
+print(board)
---
+++ main.py
@@ -0,0 +1,1 @@
+from busybee import board
