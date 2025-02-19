ac5b09ec2e977fded62d31a3132360a043d5dcfc
TR <tom06rowe@gmail.com>
2025-02-19 13:38:40 +0000

    Added turn checker

--- busybee.py
+++ busybee.py
@@ -1,7 +1,20 @@
 #This is a dictionary, which is a collection of key-value pairs. The key is the name of the variable, and the value is the value of the variable.
 
-places = {1 : 'A', 2 : 'B', 3 : 'C', 4 : 'D', 5 : 'E', 6 : "F", 7 : 'G', 8 : 'H', 9 : 'I'}
+def loadboard(places):
+  
+  board = f"{places[1]}|{places[2]}|{places[3]}\n{places[4]}|{places[5]}|{places[6]}\n{places[7]}|{places[8]}|{places[9]}"
+  print(board)
 
-board = f"{places[1]}|{places[2]}|{places[3]}\n{places[4]}|{places[5]}|{places[6]}\n{places[7]}|{places[8]}|{places[9]}"
+def whosturn(round):
+  if round % 2 == 0:
+    return "X"
+  else:
+    return "O"
 
-print(board)
+
+
+
+
+
+  
+
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-from busybee import board
+import os
+from busybee import loadboard, whosturn
+
+places = {1 : 'A', 2 : 'B', 3 : 'C', 4 : 'D', 5 : 'E', 6 : "F", 7 : 'G', 8 : 'H', 9 : 'I'}
+
+turn = 0
+
+
+
+online = True
+
+
+while online:
+  os.system('cls' if os.name == 'nt' else 'clear')
+  loadboard(places)
+ 
+
+  if whosturn(turn) == 'X':
+    move = input("Player 1: Where do you want to move? ")
+  else:
+    move = input("Player 2: Where do you want to move? ")
+
+  turn = turn + 1
